d6f8384d74bb743d4569e30db9f5aa7582d9e40b
Melih <44797690+melihaslan@users.noreply.github.com>
2020-06-10 21:25:33 +0300

    Add files via upload

---
+++ AutomaticLiquidSoapDispenser.ino
@@ -0,0 +1,27 @@
+const int IRsensor = 8;
+const int Enable_A = 9;
+const int inputA1 = 10;
+const int inputA2 = 11;
+
+void setup() {
+  pinMode(IRsensor,INPUT);
+  pinMode(Enable_A, OUTPUT);
+  pinMode(inputA1, OUTPUT);
+  pinMode(inputA2, OUTPUT);
+}
+
+void loop() {
+  int IRvalue = digitalRead(IRsensor);
+  digitalWrite(Enable_A, HIGH);
+  if (IRvalue == 0) {
+    digitalWrite(inputA1, HIGH);
+    digitalWrite(inputA2, LOW);
+    delay(1000);
+    digitalWrite(inputA1, LOW);
+    digitalWrite(inputA2, LOW);
+    delay(2000);
+  } else {
+    digitalWrite(inputA1, LOW);
+    digitalWrite(inputA2, LOW);
+  }
+}
